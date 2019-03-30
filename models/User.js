ee0975a8f6985b022072c6e923c73c9d8c561637
Huong <huongnguyenak96@gmail.com>
2019-03-30 13:58:58 +0700

    add model User

---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    email: {type:String, required:true, unique:true}, 
+    password : {type:String, required:true}, 
+    name: String, 
+    posts: [
+        {type:Schema.Types.ObjectId, ref:'post'}
+    ], 
+    friends:[
+        {type:Schema.Types.ObjectId, ref:'user'}
+    ], 
+    receiveRequests:[
+        {type:Schema.Types.ObjectId, ref:'user'}
+    ], 
+    sendRequests:[
+        {type:Schema.Types.ObjectId, ref:'user'}
+    ], 
+})
+const User = mongoose.model('user',UserSchema)
